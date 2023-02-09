c5bc178d0e4beb197de506eff15516f2708354aa
Zirui He <zr_he@yahoo.com>
2023-02-09 14:09:29 +0800

    Update PSThermo.jl

--- src/PSThermo.jl
+++ src/PSThermo.jl
@@ -1,9 +1,7 @@
 module PSThermo
     import Printf.@sprintf
     include("data.jl")
-    include("SolidsEnthalpy.jl")
-    include("SolidsCp.jl")
-    include("SolidsEntropy.jl")
+    include("SolidsThermo.jl")
     include("GasThermo.jl")
     include("MolarMass.jl")
     include("Viscosity.jl")
