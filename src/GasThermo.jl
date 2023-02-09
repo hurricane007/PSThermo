1ea9b5a0c96abbf7db9d9c3794dbb161a0ac36fa
Zirui He <zr_he@yahoo.com>
2023-02-09 14:05:03 +0800

    Update GasThermo.jl

--- src/GasThermo.jl
+++ src/GasThermo.jl
@@ -12,7 +12,8 @@ function GasEnthalpy(subName::String, Temp::Real)
         else
             T = Temp*1E-3
             loc = findlast(x -> x<=Temp, Trange)
-            return EnthalpyNIST(T, coeff) #sum( [T, T^2/2, T^3/3, T^4/4, -1/T, 1, 0, -1] .* coeff[loc,:]) + coeff[loc, end]
+            return EnthalpyNIST(T, coeff[loc,:]) 
+            # return sum( [T, T^2/2, T^3/3, T^4/4, -1/T, 1, 0, -1] .* coeff[loc,:]) + coeff[loc, end]
         end
     catch
         printstyled(color=:red,@sprintf("%s not found in database.\n",subName))
@@ -40,7 +41,8 @@ function GasEntropy(subName::String, Temp::Real)
         else
             T = Temp*1E-3
             loc = findlast(x -> x<=Temp, Trange)
-            return EntropyNIST(T, coeff) #sum( [log(T), T, T^2/2, T^3/3, -1/(2*T^2), 0, 1, 0] .* coeff[loc,:])*1E-3
+            return EntropyNIST(T, coeff[loc,:]) 
+            # return sum( [log(T), T, T^2/2, T^3/3, -1/(2*T^2), 0, 1, 0] .* coeff[loc,:])*1E-3
         end
     catch
         printstyled(color=:red,@sprintf("%s not found in database.\n",subName))
@@ -53,7 +55,7 @@ function GasEntropy()
 end
 
 function EntropyNIST(T, p)
-    return p[1]*log(T)+p[2]*T+p[3]*t^2/2+p[4]*T^3/3-p[5]/(2*T^2)+p[7]
+    return p[1]*log(T)+p[2]*T+p[3]*t^2/2+p[4]*T^3/3-p[5]/2/T^2+p[7]
 end
 
 "return the heat capacity of substance, kJ/mol*K"
@@ -69,7 +71,8 @@ function GasCp(subName::String, Temp::Real)
         else
             T = Temp*1E-3
             loc = findlast(x -> x<=Temp, Trange)
-            return sum( [1, T, T^2, T^3, 1/T^2, 0, 0, 0] .* coeff[loc,:])*1E-3
+            return CpNIST(T, coeff[loc,:]) 
+            # return sum( [1, T, T^2, T^3, 1/T^2, 0, 0, 0] .* coeff[loc,:])*1E-3
         end
     catch
         printstyled(color=:red,@sprintf("%s not found in database.\n",subName))
